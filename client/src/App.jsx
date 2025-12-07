ae4a04a2025b38de3a0fd482d1e4fb9403275ea3
Lahiru Niroshan <e20272@eng.pdn.ac.lk>
2025-12-07 14:12:38 +0530

    complete listing create ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import About from './pages/About';
 import Profile from './pages/Profile';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
+import CreateListing from './pages/CreateListing';
 import Header from './components/Header';
 import PrivateRoute from './components/PrivateRoute';
 
@@ -16,9 +17,11 @@ export default function App() {
         <Route path='/about' element={<About />} />
         <Route element={<PrivateRoute />}>
           <Route path='/profile' element={<Profile />} />
+          <Route path='/create-listing' element={<CreateListing />} />
         </Route>
         <Route path='/signin' element={<SignIn />} />
         <Route path='/signup' element={<SignUp />} />
+        
       </Routes>
     </BrowserRouter>
   );
