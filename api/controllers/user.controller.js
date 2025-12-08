10cd4575335c5a2f3b327355dbbf2c040579b2a3
Lahiru Niroshan <e20272@eng.pdn.ac.lk>
2025-12-08 21:18:23 +0530

    create get user listings api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import bcryptptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
 import User from "../models/user.models.js";
+import Listing from "../models/listing.models.js";
 
 export const test = (req, res) => {
   res.json({ message: "Api route is work!" });
@@ -37,6 +38,17 @@ export const deleteUser = async (req, res,next) => {
     await User.findByIdAndDelete(req.params.id);
     res.clearCookie("access_token");
     res.status(200).json("User has been deleted.");
+  } catch (error) {
+    next(error);
+  }
+}
+
+export const getUserListing = async (req, res,next) => {
+  if (req.user.id !== req.params.id) return next(errorHandler(401, "You can get only your account listing"));
+
+  try {
+    const listing = await Listing.find({ userRef: req.params.id });
+    res.status(200).json(listing);
   } catch (error) {
     next(error);
   }
